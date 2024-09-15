33415c9125341764693d633227cc2c5dd3a03568
AnkitJ393 <jangidankit767@gmail.com>
2024-09-16 02:35:00 +0530

    resolved updatedPromptPage

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import UpdatePromptPage from "./UpdatePromptPage";
 
 
-export default function UpdatePromptPage() {
+export default function UpdatePrompt() {
   return (
     <Suspense fallback={<div>Loading prompt details...</div>}>
       <UpdatePromptPage />
